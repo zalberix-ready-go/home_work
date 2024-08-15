8ae02ff7bfd3fdd0f80f33a72de2a57fdc8add23
Zalberix <zalberix@gmail.com>
2024-08-16 00:00:45 +0500

    Добавлена простая распаковка строк

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/zalberix-ready-go/home_work/hw02_unpack_string
 
 go 1.22
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var repeatChar string
+	var unpucked strings.Builder
+
+	for _, v := range input {
+		char := string(v)
+
+		if repeatChar == "" {
+			if _, err := strconv.Atoi(char); err == nil {
+				return "", ErrInvalidString
+			}
+			repeatChar = char
+			continue
+		}
+
+		multiplier, err := strconv.Atoi(char)
+		if err != nil {
+			unpucked.WriteString(repeatChar)
+			repeatChar = char
+			continue
+		}
+
+		repeatString := strings.Repeat(repeatChar, multiplier)
+
+		unpucked.WriteString(repeatString)
+
+		repeatChar = ""
+	}
+
+	// last char
+	unpucked.WriteString(repeatChar)
+
+	return unpucked.String(), nil
 }
