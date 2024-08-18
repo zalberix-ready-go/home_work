c3f4f81aff8311fc6991ed0faba6e93db0f9cdf5
Zalberix <46089841+Zalberix@users.noreply.github.com>
2024-08-18 15:13:19 +0500

    Hw02 unpack string
    * Добавлена простая распаковка строк
    
    * Добавлена обработка с экранированием символов
    
    * Для тестов отключена проверка на разрешонные пакеты, в том числе для того чтобы использовать стронние библиотеки для тестирования.
    
    * Добавлен кейс с знаком экранирования в конце

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,89 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var repeatChar string
+	var unpucked strings.Builder
+
+	nextCharEscape := false
+
+	for _, v := range input {
+		char := string(v)
+
+		if char == `\` {
+			if !nextCharEscape {
+				nextCharEscape = true
+				continue
+			}
+
+			// Если есть символ до экранированного, то его надо сохранить
+			if repeatChar != "" {
+				unpucked.WriteString(repeatChar)
+			}
+
+			repeatChar = char
+
+			nextCharEscape = false
+
+			continue
+		}
+
+		if nextCharEscape {
+			// Если есть символ до экранированного, то его надо сохранить
+			if repeatChar != "" {
+				unpucked.WriteString(repeatChar)
+			}
+
+			// Если хотят экранировать не число, то это ошибка
+			_, err := strconv.Atoi(char)
+			if err != nil {
+				return "", ErrInvalidString
+			}
+
+			repeatChar = char
+
+			nextCharEscape = false
+
+			continue
+		}
+
+		if repeatChar == "" {
+			if _, err := strconv.Atoi(char); err == nil {
+				return "", ErrInvalidString
+			}
+			repeatChar = char
+			continue
+		}
+
+		multiplier, err := strconv.Atoi(char)
+		if err != nil {
+			unpucked.WriteString(repeatChar)
+
+			repeatChar = char
+
+			continue
+		}
+
+		repeatString := strings.Repeat(repeatChar, multiplier)
+
+		unpucked.WriteString(repeatString)
+
+		repeatChar = ""
+	}
+
+	// В случае если есть символ экранирования в конце
+	if nextCharEscape {
+		return "", ErrInvalidString
+	}
+
+	// last char
+	unpucked.WriteString(repeatChar)
+
+	return unpucked.String(), nil
 }
