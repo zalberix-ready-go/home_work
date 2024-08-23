45cd95429a868fc44ae4934260c8f2d569d8e46b
Zalberix <zalberix@gmail.com>
2024-08-23 22:24:52 +0500

    Домашняя работа 3: Добавлена функция с для частотного анализа (топ 10)

--- hw03_frequency_analysis/go.mod
+++ hw03_frequency_analysis/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw03_frequency_analysis
+module github.com/zalberix-ready-go/home_work/hw03_frequency_analysis
 
 go 1.22
 
@@ -7,5 +7,6 @@ require github.com/stretchr/testify v1.7.0
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,101 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"sort"
+	"strings"
+
+	"golang.org/x/text/collate"
+	"golang.org/x/text/language"
+)
+
+var collateRussian = collate.New(language.Russian)
+
+func Top10(text string) []string {
+	text = strings.ToLower(text)
+
+	words := strings.Fields(text)
+
+	words, err := normalazeWords(words)
+	if err != nil {
+		log.Panic("normalazed words: %w", err)
+
+		return nil
+	}
+
+	frequencyWords := make(map[string]int)
+
+	// Подсчет частоты слов
+	for _, word := range words {
+		// В случае если нет такого ключа то для int zero value - 0
+		frequencyWords[word]++
+	}
+
+	wordsGroupByFrequency := make(map[int][]string)
+
+	// Группировака слов по частоте появления
+	for word, count := range frequencyWords {
+		wordsGroupByFrequency[count] = append(wordsGroupByFrequency[count], word)
+	}
+
+	allGroup := make([]int, 0)
+
+	// Получение всех групп (ключей)
+	for group := range wordsGroupByFrequency {
+		allGroup = append(allGroup, group)
+	}
+
+	// Сортируем по убыванию
+	sort.Slice(allGroup, func(i, j int) bool {
+		return allGroup[i] > allGroup[j]
+	})
+
+	topFrequencyWord := make([]string, 0)
+
+	for _, group := range allGroup {
+		collateRussian.SortStrings(wordsGroupByFrequency[group])
+
+		topFrequencyWord = append(topFrequencyWord, wordsGroupByFrequency[group]...)
+
+		if len(topFrequencyWord) >= 10 {
+			break
+		}
+	}
+
+	if len(topFrequencyWord) > 10 {
+		return topFrequencyWord[:10]
+	}
+
+	return topFrequencyWord
+}
+
+// Убирает все знаки препинания по бокам
+
+func normalazeWords(words []string) ([]string, error) {
+	var err error
+
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("произошла паника при компиляции регулярного выражения: %v", r)
+		}
+	}()
+
+	re := regexp.MustCompile(`^[^а-яА-ЯёЁa-zA-Z0-9]+|[^а-яА-ЯёЁa-zA-Z0-9]+$`)
+
+	normalWords := make([]string, 0, len(words))
+
+	for _, word := range words {
+		if word == "-" {
+			continue
+		}
+		normalWord := re.ReplaceAllString(word, "")
+		if normalWord != "" {
+			normalWords = append(normalWords, normalWord)
+		} else {
+			normalWords = append(normalWords, word)
+		}
+	}
+	return normalWords, err
 }
