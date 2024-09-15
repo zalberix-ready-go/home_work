077806caa8781600d6b725c1269c4dda3247dab7
Zalberix <zalberix@gmail.com>
2024-09-15 18:53:12 +0500

    Исправлено потоко не безопасность к данным LRU кэша

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -19,7 +19,7 @@ type lruCache struct {
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
-	mutex    sync.RWMutex
+	mutex    sync.Mutex
 }
 
 func NewCache(capacity int) Cache {
@@ -31,54 +31,43 @@ func NewCache(capacity int) Cache {
 }
 
 func (cache *lruCache) Set(key Key, value interface{}) bool {
-	cache.mutex.RLock()
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+
 	item, ok := cache.items[key]
-	cache.mutex.RUnlock()
+
+	queueItem := QueueItem{
+		key,
+		value,
+	}
 
 	if !ok {
-		cache.mutex.Lock()
 		if cache.queue.Len() == cache.capacity {
 			lastEl := cache.queue.Back()
 			lastElKey := lastEl.Value.(QueueItem).Key
 			cache.queue.Remove(lastEl)
 			delete(cache.items, lastElKey)
 		}
-		cache.mutex.Unlock()
 
-		queueItem := QueueItem{
-			key,
-			value,
-		}
-
-		cache.mutex.Lock()
 		cache.items[key] = cache.queue.PushFront(queueItem)
-		cache.mutex.Unlock()
 	} else {
-		item.Value = QueueItem{
-			key,
-			value,
-		}
-
-		cache.mutex.Lock()
+		item.Value = queueItem
 		cache.queue.MoveToFront(item)
-		cache.mutex.Unlock()
 	}
 
 	return ok
 }
 
 func (cache *lruCache) Get(key Key) (interface{}, bool) {
-	cache.mutex.RLock()
+	cache.mutex.Lock()
+	defer cache.mutex.Unlock()
+
 	item, ok := cache.items[key]
-	cache.mutex.RUnlock()
 
 	if !ok {
 		return nil, false
 	}
-
-	cache.mutex.Lock()
 	cache.queue.MoveToFront(item)
-	cache.mutex.Unlock()
 
 	return item.Value.(QueueItem).Value, true
 }
